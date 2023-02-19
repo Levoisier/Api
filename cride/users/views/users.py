eebce5f04baa6c8fe9d15edca009806f62f00b4a
Cristian Zapata <c.zapata.iq@gmail.com>
2023-02-19 11:35:09 -0500

    Login/Sign up views create, psw validations

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -3,9 +3,14 @@
 # Django REST Framework
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Serializers
-from cride.users.serializers import UserLoginSerializer
+from cride.users.serializers import (
+    UserLoginSerializer,
+    UserModelSerializer,
+    UserSignUpSerializer,
+)
 
 class UserLoginAPIView(APIView):
     """User Login API view"""
@@ -14,10 +19,24 @@ class UserLoginAPIView(APIView):
         """Handle HTTP POST Request"""
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        token = serializer.save()
+        user, token = serializer.save()
         data = {
-            'status': 'OK',
-            'token': token
+            'user': UserModelSerializer(user).data,
+            'access_token': token
         }
+
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserSignUpAPIView(APIView):
+    """User Sign up API view"""
+
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST Request"""
+
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
 
         return Response(data, status=status.HTTP_201_CREATED)
